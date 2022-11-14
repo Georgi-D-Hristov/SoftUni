d26e84e607a800b6966adeed57783a8ed0ef390e
Georgi Hristov <georgi.dh@gmail.com>
2022-11-14 23:13:39 +0200

    Add skeleton to the repository.

---
+++ C# Web Development Basics - Exams/Retake Exam - 19 Apr 2022/01. Bus Station_Skeleton - 5.0/BusStation/BusStation/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace BusStation
+{
+    using MyWebServer;
+    using System.Threading.Tasks;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using Microsoft.EntityFrameworkCore;
+    using BusStation.Data;
+
+    public class Startup
+    {
+        public static async Task Main()
+           => await HttpServer
+               .WithRoutes(routes => routes
+                   .MapStaticFiles()
+                   .MapControllers())
+               .WithServices(services => services
+               .Add<BusStationDbContext>()
+               .Add<IViewEngine, CompilationViewEngine>())
+               .WithConfiguration<BusStationDbContext>(context => context
+                   .Database.Migrate())
+               .Start();
+    }
+}
