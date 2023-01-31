789adb6ad981e5295f1b78c51cd1c6df61c8bb5c
Georgi Hristov <georgi.dh@gmail.com>
2023-01-31 23:30:48 +0200

    Add the second and the third tasks.

--- C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/02.BasicQueueOperations/Program.cs
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/02.BasicQueueOperations/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var firstLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var secondLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        var numberOfElements = firstLine[0];
+        var elementsToDequeue = firstLine[1];
+        var searchElement = firstLine[2];
+
+        var queue = new Queue<int>();
+
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            queue.Enqueue(secondLine[i]);
+        }
+
+        for (int j = 0; j < elementsToDequeue; j++)
+        {
+            queue.Dequeue();
+        }
+
+        if (queue.Count==0)
+        {
+            Console.WriteLine(0);
+        }
+        else if (queue.Contains(searchElement)) 
+        {
+            Console.WriteLine("true");
+        }
+        else 
+        {
+            Console.WriteLine(queue.Min());
+        }
+    }
+}
---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numOfQueries = int.Parse(Console.ReadLine());
+
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < numOfQueries; i++)
+        {
+            var command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            switch (command[0])
+            {
+                case 1:
+                    stack.Push(command[1]);
+                    break;
+                case 2:
+                    if (stack.Count == 0)
+                    {
+                        break;
+                    }
+                    stack.Pop();
+                    break;
+                case 3:
+                    if (stack.Count == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(stack.Max());
+                    break;
+                case 4:
+                    if (stack.Count == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(stack.Min());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", stack));
+    }
+}
