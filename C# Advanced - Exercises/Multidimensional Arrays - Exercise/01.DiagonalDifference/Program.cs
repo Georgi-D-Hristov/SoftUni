723f81cb5379caff80da3ebc2f3a464f53ba0632
Georgi Hristov <georgi.dh@gmail.com>
2023-02-09 23:53:53 +0200

    Start Multidimensional Array Exercises.

---
+++ C# Advanced - Exercises/Multidimensional Arrays - Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,31 @@
+﻿var matrixSize = int.Parse(Console.ReadLine());
+
+var matrix = new int[matrixSize, matrixSize];
+
+for (int irow = 0; irow < matrixSize; irow++)
+{
+    var inputLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+	for (int col = 0; col < matrixSize; col++)
+	{
+		matrix[irow,col] = inputLine[col];
+	}
+}
+
+var primaryDiagonal = 0;
+var secondaryDiagonal = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+	primaryDiagonal += matrix[i,i];
+}
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    secondaryDiagonal+= matrix[row,matrix.GetLength(0)-1-row];
+}
+
+
+var absoluteDifference = Math.Abs(primaryDiagonal - secondaryDiagonal);
+
+Console.WriteLine(absoluteDifference);
