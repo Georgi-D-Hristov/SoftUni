2545dbf8eb108185f065b5dd7745c30e185e50e6
Georgi Hristov <georgi.dh@gmail.com>
2023-03-28 00:07:01 +0300

    Task02 is solved.

--- C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/01.UniqueUsernames/Program.cs
+++ C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/01.UniqueUsernames/Program.cs
@@ -1,10 +1,10 @@
 ﻿var uniqueNames = new HashSet<string>();
 var numberOfNames = int.Parse(Console.ReadLine());
 
-for(int i = 0; i < numberOfNames; i++)
+for (int i = 0; i < numberOfNames; i++)
 {
     var name = Console.ReadLine();
     uniqueNames.Add(name);
 }
 
-Console.WriteLine(string.Join("\n",uniqueNames));
+Console.WriteLine(string.Join("\n", uniqueNames));
