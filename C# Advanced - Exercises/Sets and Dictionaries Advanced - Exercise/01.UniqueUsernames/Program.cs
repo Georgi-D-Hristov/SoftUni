875e5f20582325337110499292bf29b7cf92e5ff
Georgi Hristov <georgi.dh@gmail.com>
2023-03-27 23:50:46 +0300

    Create new folder and solved the first task.

---
+++ C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,10 @@
+﻿var uniqueNames = new HashSet<string>();
+var numberOfNames = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < numberOfNames; i++)
+{
+    var name = Console.ReadLine();
+    uniqueNames.Add(name);
+}
+
+Console.WriteLine(string.Join("\n",uniqueNames));
