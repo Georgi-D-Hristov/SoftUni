4119b835ef6d923e0183c4f020112a8777505ad5
Georgi Hristov <Georgi.dh@gmail.com>
2023-04-28 00:05:01 +0300

    Task 04 is done.

--- C# OOP - Exercises/Inheritance - Exercise/NeedForSpeed/StartUp.cs
+++ C# OOP - Exercises/Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -1,10 +1,15 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            var sportCar = new SportCar(250,600);
+
+            sportCar.Drive(10);
+            Console.WriteLine(sportCar.Fuel);
         }
     }
 }
---
+++ C# OOP - Exercises/Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace NeedForSpeed
+{
+
+    public abstract class Vehicle
+    {
+        public const double DEFAULT_FUEL_CONSUMPTION = 1.25;
+        protected Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public double Fuel { get; set; }
+
+
+        public int HorsePower { get; set; }
+
+        public virtual double DefaultFuelConsumption { get; set; } = DEFAULT_FUEL_CONSUMPTION;
+
+        public virtual double FuelConsumption { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            FuelConsumption = kilometers * DefaultFuelConsumption;
+            Fuel -= FuelConsumption;
+        }
+    }
+}
