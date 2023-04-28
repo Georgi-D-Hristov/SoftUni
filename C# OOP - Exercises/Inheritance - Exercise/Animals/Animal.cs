4d43db1515b33ce25e8c4d44722e9eae944c3425
Georgi Hristov <Georgi.dh@gmail.com>
2023-04-29 01:06:01 +0300

    Created hierarchy of Task 06.

---
+++ C# OOP - Exercises/Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,59 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public class Animal
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        private int age;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+        public string Name { get; set; }
+
+        public int Age 
+        { 
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0) 
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                age = value;
+            }
+        }
+
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return string.Empty;
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+            output.AppendLine($"{typeof(Animal).Name}");
+            output.AppendLine($"{Name} {Age} {Gender}");
+            output.AppendLine($"{ProduceSound}");
+
+            return output.ToString().Trim();
+        }
+    }
+}
--- C# OOP - Exercises/Inheritance - Exercise/Animals/StartUp.cs
+++ C# OOP - Exercises/Inheritance - Exercise/Animals/StartUp.cs
@@ -1,10 +1,35 @@
 ﻿namespace Animals
 {
+    using System;
+    using System.Linq;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine();
+
+            while (input != "Beast!")
+            {
+                var animalType = input;
+                var animalInfo = Console.ReadLine().Split().ToArray();
+                var name = animalInfo[0];
+                var age = int.Parse(animalInfo[1]);
+                var gender = animalInfo[2];
+
+                try
+                {
+                    var animal = new Animal(name, age, gender);
+                    Console.WriteLine(animal);
+                }
+                catch (ArgumentException mesage)
+                {
+
+                    Console.WriteLine(mesage);
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
