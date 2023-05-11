8b955aa9f099fffb02054a516654e5aeb1d31e58
Georgi Hristov <Georgi.dh@gmail.com>
2023-05-12 01:52:03 +0300

    Add Cat controller and cat add view.

---
+++ C# Web Exercises/INTRODUCTION TO ASP.NET CORE/Fluffy Duffy Munchkin Cats/Fluffy Duffy Munchkin Cats WebApp/Controllers/CatController.cs
@@ -0,0 +1,12 @@
+﻿namespace Fluffy_Duffy_Munchkin_Cats_WebApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CatController : Controller
+    {
+        public IActionResult Add() 
+        {
+            return View();
+        }
+    }
+}
