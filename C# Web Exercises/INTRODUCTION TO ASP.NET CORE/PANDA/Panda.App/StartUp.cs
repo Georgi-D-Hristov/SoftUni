26f30301a76d785a8678eef426e1763f987753cf
Georgi Hristov <Georgi.dh@gmail.com>
2023-05-26 00:28:42 +0300

    Update project.

--- C# Web Exercises/INTRODUCTION TO ASP.NET CORE/PANDA/Panda.App/StartUp.cs
+++ C# Web Exercises/INTRODUCTION TO ASP.NET CORE/PANDA/Panda.App/StartUp.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -9,17 +12,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseDeveloperExceptionPage();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
 
 app.UseAuthorization();
 
-//app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
