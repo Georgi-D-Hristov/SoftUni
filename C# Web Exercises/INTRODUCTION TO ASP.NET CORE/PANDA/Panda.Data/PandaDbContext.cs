fe82958965e34c3ba770757d96f0a1b6dbd142ac
Georgi Hristov <Georgi.dh@gmail.com>
2023-05-26 23:45:57 +0300

    Create Panda.Data project.

---
+++ C# Web Exercises/INTRODUCTION TO ASP.NET CORE/PANDA/Panda.Data/PandaDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Panda.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class PandaDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
