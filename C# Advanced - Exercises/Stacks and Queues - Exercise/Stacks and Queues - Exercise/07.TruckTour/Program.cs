38c0a2cb64f80e2ef7edd18fbb87decb69159704
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-11 00:37:05 +0200

    The first version of this solution is only with one OK test.

--- C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/07.TruckTour/Program.cs
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/07.TruckTour/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿var inputLines = int.Parse(Console.ReadLine());
+var tour = new Queue<string>();
+var indexPump = 0;
+var tank = 0;
+
+for (int i = 0; i < inputLines; i++)
+{
+    var pumpStop = Console.ReadLine();
+
+    tour.Enqueue(pumpStop);
+}
+
+while (tour.Count > 0)
+{
+    var currentStop = tour.Peek();
+
+    var pumpArgs = currentStop.Split().Select(int.Parse).ToArray();
+    var petrol = pumpArgs[0];
+    var distance = pumpArgs[1];
+
+    tank += petrol;
+
+    if (tank < distance)
+    {
+        indexPump++;
+        tank = 0;
+        tour.Dequeue();
+        tour.Enqueue(currentStop);
+    }
+    else
+    {
+        tank -= distance;
+        tour.Dequeue();
+    }
+}
+
+Console.WriteLine(indexPump);
