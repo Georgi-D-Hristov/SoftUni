60a236883e8c7279f5ed9fde9f2b7b5d14d28bc4
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-10 23:36:14 +0200

    Optimise queue enqueue and remove unused variables.

--- C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/02.BasicQueueOperations/Program.cs
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/02.BasicQueueOperations/Program.cs
@@ -5,31 +5,25 @@
         var firstLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
         var secondLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-        var numberOfElements = firstLine[0];
         var elementsToDequeue = firstLine[1];
         var searchElement = firstLine[2];
 
-        var queue = new Queue<int>();
-
-        for (int i = 0; i < numberOfElements; i++)
-        {
-            queue.Enqueue(secondLine[i]);
-        }
+        var queue = new Queue<int>(secondLine);
 
         for (int j = 0; j < elementsToDequeue; j++)
         {
             queue.Dequeue();
         }
 
-        if (queue.Count==0)
+        if (queue.Count == 0)
         {
             Console.WriteLine(0);
         }
-        else if (queue.Contains(searchElement)) 
+        else if (queue.Contains(searchElement))
         {
             Console.WriteLine("true");
         }
-        else 
+        else
         {
             Console.WriteLine(queue.Min());
         }
