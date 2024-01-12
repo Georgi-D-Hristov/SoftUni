d20837187319838931b87835b108cd42b693d064
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-13 01:51:03 +0200

    Solved task 9 only with 20 points.

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/Stacks and Queues - Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,50 @@
+﻿var text = new Stack<string>();
+var numberOfCommands = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    var commandLine = Console.ReadLine()
+        .Split()
+        .ToList();
+
+    var command = int.Parse(commandLine[0]);
+
+    if (command == 1)
+    {
+        text.Push(commandLine[1]);
+    }
+    else if (command == 2)
+    {
+        var numberOfChars = int.Parse(commandLine[1]);
+        var currentText = text.Peek();
+        if (numberOfChars>currentText.Length)
+        {
+            text.Push(string.Empty);
+        }
+        else
+        {
+            currentText=currentText.Substring(0, text.Peek().Length - numberOfChars);
+        }
+        text.Push(currentText);
+    }
+    else if (command == 3)
+    {
+        var indexOfChar = int.Parse(commandLine[1]);
+     
+        var currentText = text.Peek();
+        if (indexOfChar > currentText.Length)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine(currentText.Substring(indexOfChar - 1, 1));
+        }
+
+        
+    }
+    else if (command == 4)
+    {
+        text.Pop();
+    }
+}
