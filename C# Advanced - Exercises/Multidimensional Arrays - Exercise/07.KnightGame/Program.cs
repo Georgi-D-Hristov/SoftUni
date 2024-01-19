aea30b92f68fc5dbeb4a4ab8a0a1a6b91bfd129f
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-20 01:24:34 +0200

    Created the board on task 7 and defined the algorithm steps.

--- C# Advanced - Exercises/Multidimensional Arrays - Exercise/07.KnightGame/Program.cs
+++ C# Advanced - Exercises/Multidimensional Arrays - Exercise/07.KnightGame/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿var boaedSize = int.Parse(Console.ReadLine());
+
+var board = new char[boaedSize][];
+
+for (int i = 0; i < board.GetLength(0); i++)
+{
+    var inputData = Console.ReadLine().ToCharArray();
+    board[i] = inputData;
+}
+
+var removedKnights = 0;
+var attackingKnights =0;
+
+//1. Define attacking positions of all knights
+
+//2. Find most attacking knight
+
+//3. Remove the most attacking knight
+
+//4. Return to point 1 until attackingKnights==0
+
