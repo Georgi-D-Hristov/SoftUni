32b7c62deee822b035eec63a266c41900c6a0e09
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-28 01:57:46 +0200

    Line Numbers task is done.

--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/LineNumbers/LineNumbers.cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/LineNumbers/LineNumbers.cs
@@ -1,4 +1,7 @@
-﻿namespace LineNumbers
+﻿using System.IO;
+using System.Linq;
+
+namespace LineNumbers
 {
     using System;
     public class LineNumbers
@@ -13,7 +16,38 @@
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            var specialsSymbols = new char[] { '-', ',', '.', '!', '?', '\'', ':', ';' };
+
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    var lineCount = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        lineCount++;
+
+                        var line = reader.ReadLine();
+                        var symbols = 0;
+                        foreach (var sym in line)
+                        {
+                            if (Array.IndexOf(specialsSymbols, sym)!=-1)
+                            {
+                                symbols++;
+                            }
+                        }
+
+                        var letters = line.Split(new char[] { '-', ',', '.', '!', '?', ' ', '\'', ';', ':' },
+                            StringSplitOptions.RemoveEmptyEntries).Sum(x => x.Length);
+
+                        writer.WriteLine($"Line {lineCount}: {line} ({letters})({symbols})");
+                    }
+                }
+
+            }
         }
     }
 }
