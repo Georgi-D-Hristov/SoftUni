5920b5340d31ea2086f5378d7f88c95885c9dc9a
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-29 00:45:40 +0200

    Solved Task 3.

--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/CopyBinaryFile/CopyBinaryFile.cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/CopyBinaryFile/CopyBinaryFile.cs
@@ -1,4 +1,6 @@
-﻿namespace CopyBinaryFile
+﻿using System.IO;
+
+namespace CopyBinaryFile
 {
     using System;
     public class CopyBinaryFile
@@ -13,7 +15,19 @@
 
         public static void CopyFile(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            using (var inputStream = new FileStream(inputFilePath,FileMode.Open,FileAccess.Read))
+            {
+                using (var outputStream = new FileStream(outputFilePath,FileMode.Create,FileAccess.Write))
+                {
+                    var buffer = new byte[1024];
+                    int bufferRead;
+
+                    while ((bufferRead=inputStream.Read(buffer,0,buffer.Length))>0)
+                    {
+                        outputStream.Write(buffer,0,buffer.Length);
+                    }
+                }
+            }
         }
     }
 }
