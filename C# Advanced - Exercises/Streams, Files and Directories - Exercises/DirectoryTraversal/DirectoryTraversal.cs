d6e8df5119f130e83a38ae096780d5a521ed5b95
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-31 01:11:38 +0200

    Work on Task 4. Reading of folder is done.

--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
@@ -1,6 +1,9 @@
 ﻿namespace DirectoryTraversal
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+
     public class DirectoryTraversal
     {
         static void Main()
@@ -8,20 +11,44 @@
             string path = Console.ReadLine();
             string reportFileName = @"\report.txt";
 
-            string reportContent = TraverseDirectory(path);
+            var reportContent = TraverseDirectory(path);
             Console.WriteLine(reportContent);
 
             WriteReportToDesktop(reportContent, reportFileName);
         }
 
-        public static string TraverseDirectory(string inputFolderPath)
+        public static Dictionary<string, List<FileInfo>> TraverseDirectory(string inputFolderPath)
         {
-            throw new NotImplementedException();
+            var reportInfo = new Dictionary<string, List<FileInfo>>();
+            if (!Directory.Exists(inputFolderPath))
+            {
+                Console.WriteLine("Directory doesn`t exists.");
+            }
+
+            var folderInfo = Directory.GetFiles(inputFolderPath);
+            foreach (var file in folderInfo)
+            {
+                var fileInfo = new FileInfo(file);
+                var extension = fileInfo.Extension;
+                var fileSize= fileInfo.Length;
+
+                if (!reportInfo.ContainsKey(extension))
+                {
+                    reportInfo.Add(extension, new List<FileInfo>());
+                }
+                reportInfo[extension].Add(fileInfo);
+            }
+
+            return reportInfo;
         }
 
-        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        public static void WriteReportToDesktop(Dictionary<string, List<FileInfo>> textContent, string reportFileName)
         {
-            throw new NotImplementedException();
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), reportFileName);
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
