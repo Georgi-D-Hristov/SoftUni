f9fa20a315b89483a412208cd4175c0220c8c5bf
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-31 23:31:40 +0200

    Task 5 is done

--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
@@ -1,11 +1,12 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
     public class CopyDirectory
     {
         static void Main()
         {
-            string inputPath =  @$"{Console.ReadLine()}";
+            string inputPath = @$"{Console.ReadLine()}";
             string outputPath = @$"{Console.ReadLine()}";
 
             CopyAllFiles(inputPath, outputPath);
@@ -13,7 +14,20 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            var files = Directory.GetFiles(inputPath);
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var destination = Path.Combine(outputPath, fileName);
+                File.Copy(file, destination);
+            }
         }
     }
 }
--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/DirectoryTraversal/DirectoryTraversal.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-
-namespace DirectoryTraversal
+﻿namespace DirectoryTraversal
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
 
     public class DirectoryTraversal
     {
