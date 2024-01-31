25f620be2f3d360544b863eae3d1e56357634cf5
Georgi Hristov <Georgi.dh@gmail.com>
2024-01-31 23:59:52 +0200

    Task 6 is done

--- C# Advanced - Exercises/Streams, Files and Directories - Exercises/ZipAndExtract/ZipAndExtract .cs
+++ C# Advanced - Exercises/Streams, Files and Directories - Exercises/ZipAndExtract/ZipAndExtract .cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.IO.Compression;
     public class ZipAndExtract
     {
         static void Main()
@@ -18,12 +19,16 @@
 
         public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
         {
-            throw new NotImplementedException();
+            using var archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+            var fileName = Path.GetFileName(inputFilePath);
+            archive.CreateEntryFromFile(inputFilePath, fileName);
         }
 
         public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
         {
-            throw new NotImplementedException();
+            using var archive = ZipFile.OpenRead(zipArchiveFilePath);
+            var extract = archive.GetEntry(fileName);
+            extract.ExtractToFile(outputFilePath);
         }
     }
 }
