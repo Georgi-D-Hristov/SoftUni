ec4494cc76b50a91a3b2514ab32579b1516bc66f
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-02 00:03:03 +0200

    Task03 is done witf Func<T, T>

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,8 @@
+﻿Func<int[], int> minFunc = min => min.Min();
+
+var input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine(minFunc(input));
