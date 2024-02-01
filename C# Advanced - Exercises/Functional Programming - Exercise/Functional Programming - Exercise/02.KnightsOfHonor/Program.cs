b389807f37c3f38053a54f1f0d55a048d0cd5407
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-01 23:54:20 +0200

    Task02 is done with Action<T>

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,10 @@
+﻿var input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+Action<string> print = word => Console.WriteLine($"Sir {word}");
+
+foreach (var word in input)
+{
+    print(word);
+}
