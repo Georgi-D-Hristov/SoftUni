63c904d3658180d811022658333ea843db587248
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-02 00:26:15 +0200

    Task04 is with result 60\100.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,42 @@
+﻿var range = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var lower = range[0];
+var upper = range[1];
+
+Predicate<string> odd = command => command == "odd";
+
+var command = Console.ReadLine();
+
+if (odd(command))
+{
+    PrintOddNumbers(lower, upper);
+}
+else
+{
+    PrintEvenNumbers(lower, upper);
+}
+
+void PrintOddNumbers(int lower1, int upper1)
+{
+    for (int i = lower1; i <= upper1; i++)
+    {
+        if (i % 2 == 1)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
+
+void PrintEvenNumbers(int i1, int upper2)
+{
+    for (int j = i1; j <= upper2; j++)
+    {
+        if (j % 2 == 0)
+        {
+            Console.Write($"{j} ");
+        }
+    }
+}
