37c6ffe3928072ee7c460e120550c5d19e0b8917
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-05 17:35:48 +0200

    Task 6 is done.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+var divisor = int.Parse(Console.ReadLine());
+
+Action<int[]> print = numbers => Console.WriteLine(string.Join(" ", numbers.Reverse()));
+
+Func<int[], int[]> reverce = x => x;
+Func<int, bool> isDivisable = x => x % divisor != 0;
+
+input = input.Where(new Func<int, bool>(isDivisable)).ToArray();
+
+print(input);
