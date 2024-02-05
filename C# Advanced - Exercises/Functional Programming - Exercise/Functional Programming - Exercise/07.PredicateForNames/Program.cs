7f1c2804760e6041f4f311ef5d9d08e6b1254fee
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-05 17:44:29 +0200

    Task 7 is done.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,12 @@
+﻿var lenght = int.Parse(Console.ReadLine());
+
+Predicate<string> isNameLengthLess = name => name.Length <= lenght;
+
+Action<string[]> print = x => Console.WriteLine(string.Join("\n", x));
+
+var names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(new Func<string, bool>(isNameLengthLess))
+    .ToArray();
+
+print(names);
