809f3c0db589a64100a4f909636bff85e8453968
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-06 22:22:29 +0200

    Task 8 is done.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/08.ListOfPredicates/Program.cs
@@ -0,0 +1,33 @@
+﻿var endOfRange = int.Parse(Console.ReadLine());
+var devisors = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+var numbers = new List<int>();
+
+for (int i = 1; i <= endOfRange; i++)
+{
+    numbers.Add(i);
+}
+
+//Predicate<int> isDivider = x=>x%
+Func<int[], int, bool> predicate = (arr, i) =>
+{
+    bool isDivisable = true;
+    foreach (var devisor in devisors)
+    {
+        if (i % devisor != 0)
+        {
+            return false;
+        }
+    }
+
+    return isDivisable;
+};
+
+var results = numbers
+    .Where(number => predicate(devisors, number));
+
+Console.WriteLine(string.Join(" ", results));
+
