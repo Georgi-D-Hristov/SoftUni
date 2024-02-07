a7ed8cb06aeb71a0b9f9130c35540e550abc32cc
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-08 00:25:42 +0200

    Task 9 is done.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/09.PredicateParty/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection;
+
+var names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Party!")
+
+{
+
+    var commands = input.Split();
+    var command = commands[0];
+    var creteria = commands[1];
+    var parameter = commands[2];
+
+
+    Func<string, bool> predicate = GetPredicate(creteria, parameter);
+
+    switch (command)
+    {
+        case "Double":
+            names = Double(names, predicate);
+            break;
+        case "Remove":
+            names = Remove(names, predicate);
+            break;
+    }
+}
+
+if (!names.Any())
+{
+    Console.WriteLine("Nobody is going to the party!");
+}
+else
+{
+    Console.WriteLine($"{string.Join(", ", names)} are going to the party!");
+}
+
+Func<string, bool> GetPredicate(string command, string parameter)
+{
+    if (command == "StartsWith")
+    {
+        return x => x.StartsWith(parameter);
+
+    }
+
+    if (command == "EndsWith")
+    {
+        return x => x.EndsWith(parameter);
+    }
+
+    if (command == "Length")
+    {
+        return x => x.Length == int.Parse(parameter);
+    }
+
+    return default;
+}
+
+static List<string> Double(List<string> guestList, Func<string, bool> predicate)
+{
+    var result = new List<string>();
+
+    foreach (var guest in guestList)
+    {
+        if (predicate(guest))
+        {
+            result.Add(guest);
+        }
+        result.Add(guest);
+    }
+    return result;
+}
+
+static List<string> Remove(List<string> guestList, Func<string, bool> predicate)
+{
+    var result = guestList.Where(name => predicate(name) == false).ToList();
+    return result;
+}
