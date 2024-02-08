2fbbd1e0709e13d5ab7bd5868d803113fd3e5d2b
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-09 01:32:15 +0200

    Task 10 is start but need to clear the logic.

---
+++ C# Advanced - Exercises/Functional Programming - Exercise/Functional Programming - Exercise/10.PartyReservationModule/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+var inputNames = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+var resulList = new List<string>();
+foreach (var name in inputNames)
+{
+    resulList.Add(name);
+}
+
+string inputCommands;
+while ((inputCommands = Console.ReadLine()) != "Print")
+{
+    var commands = inputCommands.Split(";", StringSplitOptions.RemoveEmptyEntries);
+    var command = commands[0];
+    var filter = commands[1];
+    var parameter = commands[2];
+
+    Func<string, bool> predicate = GetPredicate(filter, parameter);
+
+    switch (command)
+    {
+        case "Add filter":
+            inputNames = Add(inputNames, resulList, predicate);
+            break;
+        case "Remove filter":
+            inputNames = Remove(inputNames, resulList, predicate);
+            break;
+    }
+}
+
+Console.WriteLine(string.Join(" ", inputNames));
+
+Func<string, bool> GetPredicate(string filter, string parameter)
+{
+    if (filter == "Starts with")
+    {
+        return x => x.StartsWith(parameter);
+    }
+    else if (filter == "Ends with")
+    {
+        return x => x.EndsWith(parameter);
+    }
+    else if (filter == "Length")
+    {
+        return x => x.Length == int.Parse(parameter);
+    }
+    else if (filter == "Contains")
+    {
+        return x => x.Contains(parameter);
+    }
+
+    return default;
+}
+
+static List<string> Add(List<string> guestList, List<string>resultList, Func<string, bool> predicate)
+{
+
+   return resultList = resultList.Where(x => predicate(x)==false).ToList();
+}
+
+static List<string> Remove(List<string> guestList, List<string> resultList, Func<string, bool> predicate)
+{
+    foreach (var guest in guestList)
+    {
+        if (predicate(guest))
+        {
+            resultList.Add(guest);
+        }
+    }
+
+    return resultList;
+}
