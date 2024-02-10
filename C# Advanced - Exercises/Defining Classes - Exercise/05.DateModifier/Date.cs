bc0ddebbb0f04e5bac177d294f80ed8b7944dd16
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-10 23:02:44 +0200

    Task05 is done.

---
+++ C# Advanced - Exercises/Defining Classes - Exercise/05.DateModifier/Date.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.DateModifier;
+
+using System;
+
+public class Date
+{
+    private string startDate;
+    private string endDate;
+
+    public Date(string startDate, string endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    public string EndDate
+    {
+        get { return endDate; }
+        set { endDate = value; }
+    }
+
+
+    public string StartDate
+    {
+        get { return startDate; }
+        set { startDate = value; }
+    }
+
+    public int GetDateDifference()
+    {
+        var startDate = DateTime.Parse(StartDate);
+        var endDate = DateTime.Parse(EndDate);
+        var diference = Math.Abs((startDate - endDate).Days);
+        return diference;
+    }
+}
---
+++ C# Advanced - Exercises/Defining Classes - Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.DateModifier;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var firstDate = Console.ReadLine();
+        var secondDate = Console.ReadLine();
+
+        var dates = new Date(firstDate, secondDate);
+        var result = dates.GetDateDifference();
+        Console.WriteLine(result);
+    }
+}
