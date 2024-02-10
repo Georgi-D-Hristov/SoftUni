e75f2d6d5ab53fd26ea420043e72d2da138ee977
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-10 23:55:19 +0200

    Task06 is done.

---
+++ C# Advanced - Exercises/Defining Classes - Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,66 @@
+﻿namespace _06.SpeedRacing;
+
+public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double fuelConsumptionPerKilometer;
+    private double travelledDistance;
+
+    public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+    {
+        Model = model;
+        FuelAmount = fuelAmount;
+        FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        TravelledDistance = 0;
+    }
+
+    public double FuelConsumptionPerKilometer
+    {
+        get { return fuelConsumptionPerKilometer; }
+        set { fuelConsumptionPerKilometer = value; }
+    }
+
+    public double TravelledDistance
+    {
+        get { return travelledDistance; }
+        set { travelledDistance = value; }
+    }
+
+    public double FuelAmount
+    {
+        get { return fuelAmount; }
+        set { fuelAmount = value; }
+    }
+
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+    private void CalcTravelledDistance(double amountOfKm)
+    {
+        TravelledDistance += amountOfKm;
+    }
+    public void Drive(double distance)
+    {
+        var possibleDriveDistance = FuelAmount / fuelConsumptionPerKilometer;
+
+        if (possibleDriveDistance >= distance)
+        {
+            CalcTravelledDistance(distance);
+            FuelAmount -= (FuelConsumptionPerKilometer * distance);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+
+    public override string? ToString()
+    {
+        return $"{Model} {FuelAmount:f2} {TravelledDistance}";
+    }
+}
---
+++ C# Advanced - Exercises/Defining Classes - Exercise/06.SpeedRacing/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.SpeedRacing;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var num = int.Parse(Console.ReadLine());
+        var cars = new List<Car>();
+
+        for (int i = 0; i < num; i++)
+        {
+            var carInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries); 
+            var model = carInfo[0];
+            var fuelAmount = double.Parse(carInfo[1]);
+            var fuelConsumption = double.Parse(carInfo[2]);
+
+            var car = new Car(model, fuelAmount, fuelConsumption);
+            cars.Add(car);
+        }
+
+        string command;
+        while ((command=Console.ReadLine())!="End")
+        {
+            var commandInfo = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var model = commandInfo[1];
+            var distance = double.Parse(commandInfo[2]);
+
+            var car = cars.Where(c=>c.Model == model).FirstOrDefault();
+            car.Drive(distance);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
