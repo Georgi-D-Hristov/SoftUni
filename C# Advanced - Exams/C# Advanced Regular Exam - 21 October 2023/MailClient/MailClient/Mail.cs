dc8980d43b6f8ce5d1ff1b284ed76f3981942df4
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-14 00:06:12 +0200

    Solved Task03.

--- C# Advanced - Exams/C# Advanced Regular Exam - 21 October 2023/MailClient/MailClient/Mail.cs
+++ C# Advanced - Exams/C# Advanced Regular Exam - 21 October 2023/MailClient/MailClient/Mail.cs
@@ -1,6 +1,27 @@
 ﻿namespace MailClient
 {
+    using System.Text;
+
     public class Mail
     {
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Body { get; set; }
+
+        public override string? ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"From: {Sender} / To: {Receiver}");
+            sb.AppendLine($"Message: {Body}");
+
+            return sb.ToString().Trim();
+        }
     }
 }
--- C# Advanced - Exams/C# Advanced Regular Exam - 21 October 2023/MailClient/MailClient/MailBox.cs
+++ C# Advanced - Exams/C# Advanced Regular Exam - 21 October 2023/MailClient/MailClient/MailBox.cs
@@ -1,6 +1,62 @@
 ﻿namespace MailClient
 {
+    using System.Text;
+
     public class MailBox
     {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Mail> Inbox { get; set; }
+        public List<Mail> Archive { get; set; }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Inbox.Count==Capacity)
+            {
+                return;
+            }
+            Inbox.Add(mail);
+        }
+        public bool DeleteMail(string sender)
+        {
+            if (Inbox.Any(m=>m.Sender == sender))
+            {
+                Inbox.Remove(Inbox.First(s=>s.Sender == sender));
+                return true;
+            }
+            return false;
+        }
+        public int ArchiveInboxMessages()
+        {
+            var mailCount = Inbox.Count;
+
+            foreach (var mail in Inbox)
+            {
+                Archive.Add(mail);
+            }
+            Inbox.RemoveRange(0, mailCount);
+
+            return mailCount;
+        }
+        public string GetLongestMessage()
+        {
+            return Inbox.OrderByDescending(m => m.Body.Length).First().ToString();
+        }
+        public string InboxView()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Inbox:");
+            foreach(var mail in Inbox)
+            {
+                sb.AppendLine(mail.ToString());
+            }
+            return sb.ToString().Trim();
+        }
     }
 }
