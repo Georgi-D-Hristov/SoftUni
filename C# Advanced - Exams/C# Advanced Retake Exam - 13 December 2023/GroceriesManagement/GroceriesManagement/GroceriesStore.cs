f11164f79a95945e9ac3c2ad3fd5bf21ca4b8c90
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-13 22:38:04 +0200

    Create folder for Exams and solve the task03 from the last one.

---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 13 December 2023/GroceriesManagement/GroceriesManagement/GroceriesStore.cs
@@ -0,0 +1,76 @@
+﻿namespace GroceriesManagement
+{
+    using System.Text;
+
+    public class GroceriesStore
+    {
+        public GroceriesStore(int capacity)
+        {
+            Capacity = capacity;
+            Turnover = 0;
+            Stall = new List<Product>();
+        }
+
+        public int Capacity { get; set; }
+        public double Turnover { get; set; }
+        public List<Product> Stall { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            if (Capacity == Stall.Count || Stall.Contains(product))
+            {
+                return;
+            }
+            Stall.Add(product);
+        }
+        public bool RemoveProduct(string name)
+        {
+            var product = Stall.FirstOrDefault(p => p.Name == name);
+
+            if (product != null)
+            {
+                Stall.Remove(product);
+                return true;
+            }
+            return false;
+        }
+
+        public string SellProduct(string name, double quantity)
+        {
+            var product = Stall.FirstOrDefault(p => p.Name == name);
+            if (product != null)
+            {
+                var result =Math.Round( product.Price * quantity,2);
+                Turnover+= result;
+                return $"{product.Name} - {result:f2}$";
+            }
+            return "Product not found";
+        }
+        
+        public string GetMostExpensive()
+        {
+            var product = Stall.OrderByDescending(p=>p.Price).FirstOrDefault();
+            return product.ToString();
+        }
+
+        public string CashReport()
+        {
+            return $"Total Turnover: {Turnover:F2}$";
+        }
+
+        public string PriceList()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Groceries Price List:");
+
+            foreach (var product in Stall)
+            {
+                sb.AppendLine($"{product.ToString()}");
+            }
+            
+            return sb.ToString().Trim();
+        }
+    }
+}
+
---
+++ C# Advanced - Exercises/Defining Classes - Exercise/SoftUniParking_Skeleton_6.0/Parking.cs
@@ -0,0 +1,97 @@
+﻿namespace SoftUniParking
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Parking
+    {
+		private List<Car> cars;
+		private int capacity;
+
+        public Parking(int capacity)
+        {
+            cars = new List<Car>();
+            Capacity = capacity;
+        }
+
+        public int Count => cars.Count;
+
+        public List<Car> Cars
+		{
+			get =>cars; 
+			set => cars = value; 
+		}
+
+        public int Capacity 
+        { 
+            get => capacity;
+            set => capacity = value; 
+        }
+
+        public string AddCar(Car car)
+        {
+            Car searchCar = CarExists(car.RegistrationNumber);
+            if (searchCar!=null)
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if(cars.Count>=Capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                cars.Add(car);
+               // Capacity--;
+                var result = $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+                return result ;
+            }
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            Car searchCar = CarExists(registrationNumber);
+            if (searchCar == null)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                cars.Remove(searchCar);
+                //Capacity++;
+                var result = $"Successfully removed {registrationNumber}";
+                return result;
+            }
+        }
+
+
+        public Car GetCar(string registrationNumber)
+        {
+            var car = CarExists(registrationNumber);
+            if (car!=null)
+            {
+                return car;
+            }
+            return null;
+        }
+
+       public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            foreach(var regNumber in RegistrationNumbers)
+            {
+                var car = CarExists(regNumber);
+                if(car!=null) 
+                {
+                    cars.Remove(car);
+                   // Capacity++;
+                }
+            }
+        }
+
+        private Car CarExists(string registrationNumber)
+        {
+            return cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        
+    }
+}
