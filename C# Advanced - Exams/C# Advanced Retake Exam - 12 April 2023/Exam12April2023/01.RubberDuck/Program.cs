972e38d679668ab7b0b5b160bcc7060d813edcc1
Georgi Hristov <Georgi.dh@gmail.com>
2024-02-16 21:51:30 +0200

    Exam is done with 293/300 points.

---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 12 April 2023/Exam12April2023/01.RubberDuck/Program.cs
@@ -0,0 +1,52 @@
+﻿var taskTimeInfo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+var tasksInfo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var times = new Queue<int>(taskTimeInfo);
+var tasks = new Stack<int>(tasksInfo);
+
+var vaderDuck = 0;
+var thorDuck = 0;
+var bigBlueDuck = 0;
+var smalYelowDuck = 0;
+
+
+while (times.Count>0&&tasks.Count>0)
+{
+    var time = times.Dequeue();
+    var task = tasks.Pop();
+    var taskTime = time*task;
+    if (taskTime >= 0 && taskTime <= 60)
+    {
+        vaderDuck++;
+    }
+   else if (taskTime >= 61 && taskTime <= 120)
+    {
+        thorDuck++;
+    }
+  else  if(taskTime >= 121 && taskTime <= 180)
+    {
+        bigBlueDuck++;
+    }
+   else if ((taskTime >= 181 && taskTime <= 240))
+    {
+        smalYelowDuck++;
+    }
+    else
+    {
+        tasks.Push(task - 2);
+        times.Enqueue(time);
+    }
+}
+
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+
+Console.WriteLine($"Darth Vader Ducky: {vaderDuck}");
+Console.WriteLine($"Thor Ducky: {thorDuck}");
+Console.WriteLine($"Big Blue Rubber Ducky: {bigBlueDuck}");
+Console.WriteLine($"Small Yellow Rubber Ducky: {smalYelowDuck}");
---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 12 April 2023/Exam12April2023/02.TheSquirrel/Program.cs
@@ -0,0 +1,86 @@
+﻿var size = int.Parse(Console.ReadLine());
+var matrix = new char[size, size];
+
+var commandsInfo = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+var squirrelRow = -1;
+var squirrelCol = -1;
+var nuts = 0;
+var isOutOfBorders = false;
+var isTrapped = false;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    var input = Console.ReadLine().ToCharArray();
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = input[j];
+        if (matrix[i, j] == 's')
+        {
+            squirrelRow = i;
+            squirrelCol = j;
+        }
+    }
+}
+
+foreach (var command in commandsInfo)
+{
+    switch (command)
+    {
+        case "up":
+            squirrelRow--;
+            CheckMove();
+            break;
+        case "down":
+            squirrelRow++;
+            CheckMove();
+            break;
+        case "left":
+            squirrelCol--;
+            CheckMove();
+            break;
+        case "right":
+            squirrelCol++;
+            CheckMove();
+            break;
+        default:
+            break;
+    }
+    if (isOutOfBorders || isTrapped)
+    {
+        break;
+    }
+    if (nuts == 3)
+    {
+        break;
+    }
+}
+if (nuts != 3 && !isTrapped && !isOutOfBorders)
+{
+    Console.WriteLine("There are more hazelnuts to collect.");
+}
+Console.WriteLine($"Hazelnuts collected: {nuts}");
+
+void CheckMove()
+{
+    if (squirrelRow < 0 || squirrelCol < 0 || squirrelRow >= matrix.GetLength(0) || squirrelCol >= matrix.GetLength(1))
+    {
+        Console.WriteLine("The squirrel is out of the field.");
+        isOutOfBorders = true;
+    }
+    else if (matrix[squirrelRow, squirrelCol] == 't')
+    {
+        Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+        isTrapped = true;
+    }
+    else if (matrix[squirrelRow, squirrelCol] == 'h')
+    {
+        nuts++;
+        matrix[squirrelRow, squirrelCol] = '*';
+        if (nuts == 3)
+        {
+            Console.WriteLine("Good job! You have collected all hazelnuts!");
+        }
+    }
+}
