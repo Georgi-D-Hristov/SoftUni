cc8443f112a67298b8181259c3690eb90498b56c
Georgi Hristov <Georgi.dh@gmail.com>
2024-03-21 00:49:26 +0200

    Create constructor tests on Task03.

--- C# OOP - Exercises/Unit Testing - Exercises/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
+++ C# OOP - Exercises/Unit Testing - Exercises/09. CSharp-OOP-Unit-Testing-Exercise-Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
@@ -1,10 +1,51 @@
 namespace CarManager.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class CarManagerTests
     {
+        private Car _car;
 
+        [SetUp]
+        public void Initial()
+        {
+            _car = new Car("VW", "Golf4", 6, 55);
+
+        }
+
+        [Test]
+        public void Test_CarCreate()
+        {
+            var ford = new Car("Ford", "Mustang", 25, 95);
+            Assert.IsNotNull(ford);
+            Assert.AreEqual("Ford", ford.Make);
+            Assert.AreEqual("Mustang", ford.Model);
+            Assert.AreEqual(25, ford.FuelConsumption);
+            Assert.AreEqual(95, ford.FuelCapacity);
+            Assert.AreEqual(0, ford.FuelAmount);
+        }
+
+        [Test]
+        public void Test_CarWithNotValidMake()
+        {
+            Assert.Throws<ArgumentException>(() => new Car(null, "F50", 45, 200));
+        }
+        [Test]
+        public void Test_CarWithNotValidModel()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("Ferrari", null, 45, 200));
+        }
+        [Test]
+        public void Test_CarWithNegativeFuelAmount()
+        {
+            Assert.Throws<ArgumentException>(() => _car.FuelAmount=1);
+        }
+        [Test]
+        public void Test_CarWithZeroOrNegatoveFuelConsumption()
+        {
+
+        }
     }
 }
